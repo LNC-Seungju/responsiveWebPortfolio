e83fba055f07a55a0ec33d60632e156562c1501f
홍승주 <hongsj430@gmail.com>
2021-01-04 13:06:05 +0900

    모바일 반응형 미디어 쿼리 추가

--- Responsive/eco-hosting.js
+++ Responsive/eco-hosting.js
@@ -1,3 +1,4 @@
+//  Slide Function, Button Interaction
 const slideItems = document.getElementsByClassName('slide-items');
 const slideItem = document.getElementsByClassName('slide-item');
 const slidePrev = document.getElementsByClassName('slide-prev');
@@ -18,9 +19,10 @@ slidePrev[0].addEventListener('click', ()=> {
       slideItems[0].style.transition  = '0s';
       slide(slideLength-2)
       currentIndex = slideLength-2;
-    }, 10)
+    }, 301)
   }
 })
+
 slideNext[0].addEventListener('click', ()=> {
   slideItems[0].style.transition = '0.3s';
   slide(currentIndex+1);
@@ -31,23 +33,65 @@ slideNext[0].addEventListener('click', ()=> {
       slideItems[0].style.transition  = '0s';
       slide(1)
       currentIndex = 1;
-    },101 )
+    },301 )
   }
 })
 
+// Header Interaction
 
 const header = document.getElementsByClassName('header-container');
+const pcHeaderBlank = document.getElementsByClassName('pc-header-blank');
 
 window.addEventListener('scroll', ()=> {
   if(scrollY>500) {
+    pcHeaderBlank[0].classList.add('fill');
     header[0].classList.add('setPosition')
     setTimeout(()=>{
+      pcHeaderBlank[0].classList.remove('fill');
       header[0].classList.remove('setPosition');
       header[0].classList.add('fix');
+      window.clearTimeout();
     }, 500)
   }
   else { 
+    pcHeaderBlank[0].classList.remove('fill');
     header[0].classList.remove('fix');
     header[0].classList.remove('setPosition');
+  }
+})
+
+// Mobile Header Interaction
+
+const mobileTrigger = document.getElementsByClassName('mobile-trigger');
+const mobileSubmenu = document.getElementsByClassName('mobile-submenu');
+const mobileHeader = document.getElementsByClassName('mobile-header');
+const mobileHeaderBlank = document.getElementsByClassName('mobile-header-blank');
+const submenuBlog = document.getElementsByClassName('submenu-blog');
+
+mobileTrigger[0].addEventListener('click', ()=> {
+  mobileHeader[0].classList.toggle('active');
+})
+
+submenuBlog[0].addEventListener('click', ()=> {
+  submenuBlog[0].classList.toggle('active');
+})
+
+window.addEventListener('scroll', ()=> {
+  if(scrollY == 0) {
+    mobileHeader[0].classList.remove('fix');
+  }
+  if(scrollY>500) {
+  mobileHeaderBlank[0].classList.add('fill');
+  mobileHeader[0].classList.add('setPosition')
+    setTimeout(()=>{
+      mobileHeaderBlank[0].classList.add('fill');
+      mobileHeader[0].classList.remove('setPosition');
+      mobileHeader[0].classList.add('fix');
+    }, 500)
+  }
+  else { 
+    mobileHeaderBlank[0].classList.remove('fill');
+    mobileHeader[0].classList.remove('fix');
+    mobileHeader[0].classList.remove('setPosition');
   }
 })
